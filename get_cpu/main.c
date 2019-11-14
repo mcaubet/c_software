34427286d7deacf08338e237014f61c82160b460
Caubet Serrabou Marc <caubet_m@merlin-l-001.psi.ch>
2019-11-14 11:14:57 +0100

    Added get_cpu

---
+++ get_cpu/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <omp.h>
+#include <mpi.h>
+#include "getcpuid.h"
+
+int main(int argc, char* argv[]) {
+  int numprocs, rank, namelen;
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int iam = 0, np = 1;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Get_processor_name(processor_name, &namelen);
+
+  #pragma omp parallel default(shared) private(iam, np)
+  {
+    np = omp_get_num_threads();
+    iam = omp_get_thread_num();
+    int cpu_id = get_cpu_id();
+    printf("Hello from thread %d out of %d from process %d out of %d on %s, cpu_id %d\n",
+           iam, np, rank, numprocs, processor_name, cpu_id);
+  }
+
+  MPI_Finalize();
+}
